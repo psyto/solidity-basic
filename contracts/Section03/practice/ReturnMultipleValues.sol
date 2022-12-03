35949c69395b4eaf0a16f8c30d2af365d907b9c7
psyto <saito.hiroyuki@gmail.com>
2022-12-03 09:50:09 +0900

    multiple return 2

--- contracts/Section03/practice/ReturnMultipleValues.sol
+++ contracts/Section03/practice/ReturnMultipleValues.sol
@@ -18,11 +18,17 @@ contract ReturningMultipleValues {
     }
 
     // ファンクションの複数の戻り値から必要なものだけ抽出
-
+    function throwAwayReturnValue() public pure returns (uint, bool) {
+        (uint a, , bool c) = multipleReturn();
+        return (a, c);
+    }
 
     /// @dev Function return variables（関数の戻り値変数）を利用した複数return
-
+    function add(uint a_, uint b_) public pure returns (uint mulNum, uint sumNum) {
         /// @dev returnのためだけに使う変数は型の定義不要
+        sumNum = a_ + b_;
+        mulNum = a_ * b_;
+        return (mulNum, sumNum);
+    }
 
-    
 }
