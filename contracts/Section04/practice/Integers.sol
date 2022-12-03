513d0c5652224189204bb698d3c0194be864be12
psyto <saito.hiroyuki@gmail.com>
2022-12-03 11:18:18 +0900

    bit operation

--- contracts/Section04/practice/Integers.sol
+++ contracts/Section04/practice/Integers.sol
@@ -29,32 +29,50 @@ contract Integers {
 
     /// @dev 比較演算子
     // 以下
+   bool public comparison1 = (i <= 256);
 
     // 未満
+   bool public comparison2 = (i < 256);
 
     // イコール
+   bool public comparison3 = (i == 256);
 
     // イコールではない
+   bool public comparison4 = (i != 256);
 
     // 以上
+   bool public comparison5 = (i >= 256);
 
     // より大きい
-
+   bool public comparison6 = (i > 256);
 
     /// @dev 型違いは比較できないので注意。比較したいときは型を合わせる
-
+   // bool public comparison7 = (i == u);
+   bool public comparison8 = (uint(i) == u);
 
    /// @dev ビット演算子
    // AND : 両方のビットが共に1の場合だけ1
+   int8 public iBitOpe1 = int8(12) & int8(7);
+   // 12 -> 00001100
+   //  7 -> 00000111
+   // iBitOpe1: 00000100 -> 4
 
    // OR : 両方のビットのどちらかが1の場合に1
+   int8 public iBitOpe2 = int8(12) | int8(7);
+   // 12 -> 00001100
+   //  7 -> 00000111
+   // iBitOpe2: 00001111 -> 15
 
    // XOR : ビットの値が異なる場合にだけ1
+   int8 public iBitOpe3 = int8(12) ^ int8(7);
+   // 12 -> 00001100
+   //  7 -> 00000111
+   // iBitOpe3: 00001011 -> 11
 
    // NOT : ビット反転
-
-
-
+   int8 public iBitOpe4 = ~int8(12);
+   // 12 -> 00001100
+   // iBitOpe4: 11110011 -> -112
 
    /// @dev シフト演算
    // 左シフト
