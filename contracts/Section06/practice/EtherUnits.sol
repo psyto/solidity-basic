7bb4d20057503bb923765e52729a0922d008f956
psyto <saito.hiroyuki@gmail.com>
2022-12-12 14:30:51 +0900

    unit

--- contracts/Section06/practice/EtherUnits.sol
+++ contracts/Section06/practice/EtherUnits.sol
@@ -16,14 +16,21 @@ contract EtherUnits {
         assert(1 wei == 10**0);
         return true;
     }
-
     function checkGwei() external pure returns (bool) {
         assert(1 gwei == 10**9);
         return true;
     }
-
     function checkEther() external pure returns (bool) {
         assert(1 ether == 10**18);
         return true;
+    }
+    function add1Wei(uint wei_) external pure returns (uint) {
+        return (wei_ + 1 wei);
+    }
+    function add1Gwei(uint wei_) external pure returns (uint) {
+        return (wei_ + 1 gwei);
+    }
+    function add1Ether(uint wei_) external pure returns (uint) {
+        return (wei_ + 1 ether);
     }
 }
