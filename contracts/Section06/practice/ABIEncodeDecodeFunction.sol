0c79ead714ff1aa74fb574bff18b69b350ae6505
psyto <saito.hiroyuki@gmail.com>
2022-12-13 15:45:27 +0900

    abi functions

--- contracts/Section06/practice/ABIEncodeDecodeFunction.sol
+++ contracts/Section06/practice/ABIEncodeDecodeFunction.sol
@@ -17,10 +17,19 @@ contract ABIEncodeDecodeFunction {
     //   https://docs.soliditylang.org/en/latest/050-breaking-changes.html#semantic-only-changes
 
     // encodeする。パディングあり
+    function encode(string memory s1_, string memory s2_) public pure returns (bytes memory) {
+        return abi.encode(s1_, s2_);
+    }
 
     // Solidity は非標準の packed モードをサポート。パディングなし
+    function encodePacked(string memory s1_, string memory s2_) public pure returns (bytes memory) {
+        return abi.encodePacked(s1_, s2_);
+    }
 
     // abi.encode()の結果をdecodeする
-
+    function decode(bytes memory encodedData) public pure returns (string memory, string memory) {
+        (string memory s1, string memory s2) = abi.decode(encodedData, (string, string));
+        return (s1, s2);
+    }
 
 }
