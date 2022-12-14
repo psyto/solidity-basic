f6a12bc0f4faf5cbf1caa63c3c1c19923731b2f0
psyto <saito.hiroyuki@gmail.com>
2022-12-14 10:57:25 +0900

    for loop

--- contracts/Section07/practice/For.sol
+++ contracts/Section07/practice/For.sol
@@ -7,11 +7,23 @@ pragma solidity ^0.8.17;
  */
 contract For {
 
+    uint[] array;
+    uint i;
+
     /// @dev forループ
+    function forLoop() external returns (uint[] memory) {
+        delete array;
+        for (i = 0; i <= 10; i++) {
+            array.push(i);
+        }
+        return array;
+    }
 
     /// @dev breakでloopを抜ける
 
     /// @dev continueは次のloop処理に進む
 
-
+    function getArray() external view returns (uint[] memory) {
+        return array;
+    }
 }
