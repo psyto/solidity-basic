215812ab7ea6fbf552558b58d48edc0dc77db830
psyto <saito.hiroyuki@gmail.com>
2022-12-14 10:31:37 +0900

    while loop

--- contracts/Section07/practice/While.sol
+++ contracts/Section07/practice/While.sol
@@ -6,12 +6,26 @@ pragma solidity ^0.8.17;
  * @title While を学ぼう
  */
 contract While {
-
+    uint[] array;
+    uint i;
 
     /// @dev whileループ
+    function whileLoop() external returns (uint[] memory) {
+        i = 0;
+        delete array;
+        while (i < 10) {
+            i++;
+            array.push(i);
+        }
+        return array;
+    }
 
     /// @dev breakでloopを抜ける
 
     /// @dev continueは次のloop処理に進む
+
+    function getArray() external view returns (uint[] memory) {
+        return array;
+    }
 
 }
