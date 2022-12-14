9a8ac32770a739a222df10cd589ba37b3cf2bfa3
psyto <saito.hiroyuki@gmail.com>
2022-12-14 09:59:33 +0900

    hash functions

--- contracts/Section06/practice/MathematicalCryptographicFunction.sol
+++ contracts/Section06/practice/MathematicalCryptographicFunction.sol
@@ -22,9 +22,26 @@ contract MathematicalCryptographicFunction {
      * hash化するinput dataはハッシュ後の衝突を避けるためにabi.encodeを使った方が良い。
      */
     // Keccak-256ハッシュ
+    function keccak256Hash(string memory s1_, string memory s2_) 
+        external pure returns (bytes32, bytes memory) {
+            return (keccak256(abi.encode(s1_, s2_)), abi.encode(s1_, s2_));
+    }
+
+    function keccak256Hash2(string memory s1_, string memory s2_) 
+        external pure returns (bytes32, bytes memory) {
+            return (keccak256(abi.encodePacked(s1_, s2_)), abi.encodePacked(s1_, s2_));
+    }
 
     // SHA-256ハッシュ
+    function sha256Hash(string memory s1_, string memory s2_) 
+        external pure returns (bytes32, bytes memory) {
+            return (sha256(abi.encode(s1_, s2_)), abi.encode(s1_, s2_));
+    }
 
     // RIPEMD-160ハッシュ
+    function ripemd160Hash(string memory s1_, string memory s2_) 
+        external pure returns (bytes32, bytes memory) {
+            return (ripemd160(abi.encode(s1_, s2_)), abi.encode(s1_, s2_));
+    }
 
 }
