e6bd2527df57dd43b61e4da0acc2875ffdc6db61
psyto <saito.hiroyuki@gmail.com>
2022-12-16 10:21:18 +0900

    create contract

--- contracts/Section08/practice/CreatingContract.sol
+++ contracts/Section08/practice/CreatingContract.sol
@@ -8,14 +8,32 @@ pragma solidity ^0.8.17;
 
 contract ContractA {
     /// @dev publicで定義 -> Getterファンクションを生成
+    string public name;
+    string public symbol;
+    address public owner;
+
+    constructor(string memory name_, string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+        owner = msg.sender;
+    }
 
     /// @dev トランザクション確認用ファンクション
+    function changeName(string memory newName) external returns (string memory) {
+        name = newName;
+        return name;
+    }
 
 }
 
 contract CreatingContract {
+    ContractA public conA;
 
-
+    function createContractA(string memory name_, string memory symbol_) 
+        public returns (ContractA) {
+            conA = new ContractA(name_, symbol_);
+            return conA;
+        }
     /// @dev ContractAのGetterファンクションを実行
 
     /// @dev ContractAのGetterファンクションを実行
