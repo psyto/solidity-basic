e724b9a9324ba24fff1376e0b4b58d7b10c48810
psyto <saito.hiroyuki@gmail.com>
2022-12-16 14:32:57 +0900

    function visibility

--- contracts/Section08/practice/Visibility.sol
+++ contracts/Section08/practice/Visibility.sol
@@ -48,22 +48,32 @@ contract contractA {
 // contractAを継承
 contract Visibility is contractA {
     // publicファンクションは継承先でもコントラクト内外両方からcallできる
-
+    function callPublicFunction() public pure returns (uint) {
         // コントラクト外からのファンクションcall
+        // return this.publicFunction();
 
         // コントラクト内からのファンクションcall
-
+        return publicFunction();
+    }
 
     // externalファンクションは継承先でもコントラクト外からのみしかcallできない
-
+    function callExternalFunction() public view returns (uint) {
         // コントラクト外からのファンクションcall
+        return this.externalFunction();
 
         // コントラクト内からのファンクションcall
-
+        // return externalFunction();
+    }
 
     // internalファンクションは継承先でもcallできる
+    function callInternalFunction() public pure returns (uint) {
+        return internalFunction();
+    }
 
     // privateファンクションは継承先ではcallできない
+    // function callPrivateFunction() public pure returns (uint) {
+    //     return privateFunction();
+    // }
 
     // publicな状態変数は継承先でもアクセスできる
 
