a6fdab5f457e3bde5665de7c10f838034d1f201b
psyto <saito.hiroyuki@gmail.com>
2022-12-20 14:55:48 +0900

    inheritance

--- contracts/Section09/practice/Inheritance2.sol
+++ contracts/Section09/practice/Inheritance2.sol
@@ -18,12 +18,24 @@ pragma solidity ^0.8.17;
 
 
 contract NonArgConstructor {
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
 }
 
 contract ArgConstructor {
 }
 
 /// @dev 継承したコントラクトにconstructor引数がない場合
+contract Inheritance1 is NonArgConstructor {
+  uint public price;
+
+  constructor(uint price_) {
+    price = price_;
+  }
+}
 
 /// @dev 継承したコントラクトにconstructor引数がある、かつ定数で設定したい場合
 
