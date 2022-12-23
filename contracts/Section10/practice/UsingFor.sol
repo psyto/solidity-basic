bc0cda6ee6d0aae03ccf38d5c6989b266ed8246f
psyto <saito.hiroyuki@gmail.com>
2022-12-23 14:30:24 +0900

    library

--- contracts/Section10/practice/UsingFor.sol
+++ contracts/Section10/practice/UsingFor.sol
@@ -9,5 +9,18 @@ pragma solidity ^0.8.17;
  *  ファイルレベルでも、コントラクトレベルでも、コントラクト内部で有効
  */
 
+import {IncrementCountOnly} from "./Library.sol";
 
+contract UsingFor {
+    using IncrementCountOnly for IncrementCountOnly.Counter;
+    IncrementCountOnly.Counter private _num;
+
+    function current() public view returns (uint) {
+        return _num.currentNum();
+    }
+
+    function increment() public {
+        _num.incrementNum();
+    }
+}
 
