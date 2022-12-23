4c0e839ac00b870d862af395d61ee801da8e4b29
psyto <saito.hiroyuki@gmail.com>
2022-12-23 14:21:05 +0900

    library

--- contracts/Section10/practice/Library.sol
+++ contracts/Section10/practice/Library.sol
@@ -8,4 +8,16 @@ pragma solidity ^0.8.17;
  *  ライブラリ内定義のファンクションはLibraryを使う側のコントラクトのコンテキストで実行される
  */
 
+library IncrementCountOnly {
+    struct Counter {
+        uint _num;
+    }
 
+    function currentNum(Counter storage c) internal view returns (uint) {
+        return c._num;
+    }
+
+    function incrementNum(Counter storage c) internal {
+        unchecked {c._num++;}
+    }
+}
