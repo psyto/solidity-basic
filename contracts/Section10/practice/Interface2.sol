068144ffe7e7d0dd64c9b901027ab7bbfb32c9f9
psyto <saito.hiroyuki@gmail.com>
2022-12-23 09:59:15 +0900

    interface

--- contracts/Section10/practice/Interface2.sol
+++ contracts/Section10/practice/Interface2.sol
@@ -10,6 +10,30 @@ pragma solidity ^0.8.17;
  *  3.ファンクションcall
  */
 
-
+interface ISimpleCalc {
+    function add(uint a_, uint b_) external view returns (uint);
+    function sub(uint a_, uint b_) external view returns (uint);
+    function mul(uint a_, uint b_) external view returns (uint);
+    function div(uint a_, uint b_) external view returns (uint);
+}
 
 /// @dev interfaceを継承していなければ全てのファンクションを定義する必要はない
+contract A {
+    address public immutable simpleCalcAddress;
+    constructor(address addr) {
+        simpleCalcAddress = addr;
+    }
+
+    function add(uint a_, uint b_) external view returns (uint) {
+        return ISimpleCalc(simpleCalcAddress).add(a_, b_);
+    }
+    function sub(uint a_, uint b_) external view returns (uint) {
+        return ISimpleCalc(simpleCalcAddress).sub(a_, b_);
+    }
+    function mul(uint a_, uint b_) external view returns (uint) {
+        return ISimpleCalc(simpleCalcAddress).mul(a_, b_);
+    }
+    // function div(uint a_, uint b_) external view returns (uint) {
+    //     return ISimpleCalc(simpleCalcAddress).div(a_, b_);
+    // }
+}
