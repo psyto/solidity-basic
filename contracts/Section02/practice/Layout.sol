c1852da8016c86f1377ffcf460a982c3e5ab484e
psyto <psyhiro@yahoo.co.jp>
2023-09-07 18:45:51 +0900

    test layout

--- contracts/Section02/practice/Layout.sol
+++ contracts/Section02/practice/Layout.sol
@@ -1,1 +1,11 @@
-// テストコメント
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Layout {
+    function logging() external view {
+        console.log(msg.sender);
+    }
+}
